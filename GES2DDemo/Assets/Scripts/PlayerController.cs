7bf90e68832ffe4528bf873f8ea55a05b3cc310b
IAM\Robert.Bjornsson <robertsmbj@gmail.com>
2017-09-18 18:14:25 -0500

    AddsPlayer Movement
    Adds player movement to the GEP 2D Demo unity project.

---
+++ GES2DDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+	}
+
+    public float GetHorizontal()
+    {
+        return horizontalInput;
+    }
+    public bool IsPressed(string key)
+    {
+        if(Input.GetButton(key))
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool IsPressedOnce(string key)
+    {
+        if (Input.GetButtonDown(key))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ GES2DDemo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float jumpVelocity;
+
+    PlayerController controller;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        movementSpeed = 10f;
+        jumpVelocity = 10f;
+
+        controller = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        if(rb == null)
+        {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdatePlayerMovement();
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        float horizontalInput = controller.GetHorizontal();
+        rb.velocity = new Vector2(horizontalInput * movementSpeed, rb.velocity.y);
+        //Doesn't use physics system
+        //transform.Translate(0.1f * horizontalInput, 0, 0);
+
+        if(controller.IsPressedOnce("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+        }
+    }
+}
