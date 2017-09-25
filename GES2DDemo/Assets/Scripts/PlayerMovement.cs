265e6c729fc781e5254201de54abcd3a3f86f9ac
unknown <Robert.Bjornsson@iam.local>
2017-09-25 18:20:05 -0500

    More Movement Stuff
    Adds a jump method that only allows a single jump from a Ground surface.

--- GES2DDemo/Assets/Scripts/PlayerMovement.cs
+++ GES2DDemo/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerStates { OnGround, InAir }
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed;
+    private Transform groundDetectPoint;
     [SerializeField]
-    private float jumpVelocity;
+    private float groundDetectRadius = 0.2f;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private float movementSpeed = 10f;
+    [SerializeField]
+    private float jumpVelocity = 10f;
+    private PlayerStates currentState;
 
     PlayerController controller;
     Rigidbody2D rb;
@@ -15,33 +24,52 @@ public class PlayerMovement : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        movementSpeed = 10f;
-        jumpVelocity = 10f;
-
         controller = GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
-        if(rb == null)
-        {
-            
-        }
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
     {
-        UpdatePlayerMovement();
+        //UpdateGroundState();
+        UpdateMovement();
+        UpdateJump();
     }
 
-    private void UpdatePlayerMovement()
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(currentState == PlayerStates.InAir)
+    //    {
+    //        currentState = PlayerStates.OnGround;
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        UpdateGroundState();
+    }
+    private void UpdateGroundState()
+    {
+        Vector2 colliderPoint = new Vector2(groundDetectPoint.position.x, groundDetectPoint.position.y);
+        Collider2D[] objects = Physics2D.OverlapCircleAll(colliderPoint, groundDetectRadius ,groundLayer);
+        if(objects.Length > 0 && currentState == PlayerStates.InAir)
+        {
+            currentState = PlayerStates.OnGround;
+        }
+    }
+    private void UpdateMovement()
     {
         float horizontalInput = controller.GetHorizontal();
         rb.velocity = new Vector2(horizontalInput * movementSpeed, rb.velocity.y);
         //Doesn't use physics system
         //transform.Translate(0.1f * horizontalInput, 0, 0);
-
-        if(controller.IsPressedOnce("Jump"))
+    }
+    private void UpdateJump()
+    {
+        if (controller.IsPressedOnce("Jump") && currentState == PlayerStates.OnGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                currentState = PlayerStates.InAir;
         }
     }
 }
