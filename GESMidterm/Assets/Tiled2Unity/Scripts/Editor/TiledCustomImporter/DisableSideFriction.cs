88332397f130de652628eaaab46bbc5d191f5e6b
ROBERT-PC\Robert <robertsmbj@gmail.com>
2017-10-30 03:42:47 -0500

    One Way Progress
    Makes progress on One Way Platforms.

---
+++ GESMidterm/Assets/Tiled2Unity/Scripts/Editor/TiledCustomImporter/DisableSideFriction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Tiled2Unity.CustomTiledImporter]
+public class DisableSideFriction : Tiled2Unity.ICustomTiledImporter
+{
+    public void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> keyValuePairs)
+    {
+        Debug.Log("Handle Custom properties from Tiled Map.");
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+        PolygonCollider2D[] polygon2D = prefab.GetComponentsInChildren<PolygonCollider2D>();
+        if(polygon2D == null)
+        {
+            return;
+        }
+
+        int oneWayMask = LayerMask.NameToLayer("OneWay");
+
+        foreach (PolygonCollider2D polygon in polygon2D)
+        {
+            PlatformEffector2D effector = polygon.gameObject.GetComponent<PlatformEffector2D>();
+            if(effector == null)
+            {
+                effector = polygon.gameObject.AddComponent<PlatformEffector2D>();
+            }
+
+            if(polygon.gameObject.layer == oneWayMask)
+            {
+                effector.useOneWay = true;
+            }
+            else
+            {
+                effector.useOneWay = false;
+            }
+            effector.useSideFriction = false;
+            polygon.usedByEffector = true;
+        }
+    }
+}
