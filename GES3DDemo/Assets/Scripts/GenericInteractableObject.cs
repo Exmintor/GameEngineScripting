e253a0ddf4f3af659cd5c3ae225a5bab3271e53e
Robert Bjornsson <robertsmbj@gmail.com>
2017-11-20 18:02:30 -0600

    Generic Interactable Scripts
    Adds some generic Interactable scripts to 3D project.

---
+++ GES3DDemo/Assets/Scripts/GenericInteractableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericInteractableObject : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        //What happens here?
+    }
+}
