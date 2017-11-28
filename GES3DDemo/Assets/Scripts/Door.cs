255ba822a5070645ba84e0e508f024936cc23d64
unknown <Robert.Bjornsson@iam.local>
2017-11-27 18:10:28 -0600

    Door animation
    Makes Door open and close with animation.

--- GES3DDemo/Assets/Scripts/Door.cs
+++ GES3DDemo/Assets/Scripts/Door.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-
+    private bool isOpen = false;
+    private Animator animator;
 	// Use this for initialization
 	void Start ()
     {
-		
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -19,5 +20,20 @@ public class Door : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        ReverseIsOpen();
+        animator.SetBool("isOpen", isOpen);
+    }
+
+    public void ReverseIsOpen()
+    {
+        switch(isOpen)
+        {
+            case true:
+                isOpen = false;
+                break;
+            case false:
+                isOpen = true;
+                break;
+        }
     }
 }
